adf2b2821c7c751a0255d7d2861e8817c8b97d19
Kim Daeyeon <80505979+alwaysbemyday1@users.noreply.github.com>
2023-03-20 13:36:32 +0000

    First Hash

--- Hash/ponketmon.py
+++ Hash/ponketmon.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 
+
 # 1st try
 # right but overed time
 def solution(nums):
@@ -15,12 +16,13 @@ def solution(nums):
 def solution2(nums):
   num = int(len(nums) / 2)
   new_num = int(len(list(set(nums))))
-  answer = num if num<new_num else new_num
+  answer = num if num < new_num else new_num
   return answer
 
 
 # answer
 def solution3(ls):
-    return min(len(ls)/2, len(set(ls)))
+  return min(len(ls) / 2, len(set(ls)))
 
-print(solution3([3,1,2,3]))
+
+print(solution3([3, 1, 2, 3]))
