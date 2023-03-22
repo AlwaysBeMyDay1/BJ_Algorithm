7c4e5e8175fe6acf19567ba20a34207472c756d1
Kim Daeyeon <80505979+alwaysbemyday1@users.noreply.github.com>
2023-03-22 05:08:12 +0000

    Hash_Marathon with Hash

--- Hash/marathon.py
+++ Hash/marathon.py
@@ -1,29 +1,50 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42576
 # 완주하지 못한 선수
 
+
+def solution(participant, completion):
+    new_dict = {}
+    for i in completion:
+        if new_dict.get(i):
+            new_dict[i] += 1
+        else:
+            new_dict[i] = 1
+
+    for i in participant:
+        if not new_dict.get(i):
+            return i
+        else:
+            if new_dict.get(i) == 0:
+                return i
+            else:
+                new_dict[i] -= 1
+
+
 # 1st try
 # right but overed time
-def solution(participant, completion):
+def solution1(participant, completion):
     for x in participant:
-      if x in completion:
-        completion.remove(x)
-      else:
-        return x
+        if x in completion:
+            completion.remove(x)
+        else:
+            return x
     return 'no answer'
+
 
 # 2nd try
 # right but overed time
 def solution2(participant, completion):
     for x in participant:
-      if participant.count(x) != completion.count(x):
-        return x
+        if participant.count(x) != completion.count(x):
+            return x
+
 
 # 3rd try
 # succeed
 def solution3(participant, completion):
     sorted_participant = sorted(participant)
     sorted_completion = sorted(completion)
-    
+
     for index, x in enumerate(sorted_participant):
         try:
             if x != sorted_completion[index]:
@@ -35,12 +56,14 @@ def solution3(participant, completion):
 # answer 1
 import collections
 
-def solution(participant, completion):
+
+def solution_a1(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     return list(answer.keys())[0]
 
+
 # answer 2
-def solution(participant, completion):
+def solution_a2(participant, completion):
     answer = ''
     temp = 0
     dic = {}
@@ -52,5 +75,3 @@ def solution(participant, completion):
     answer = dic[temp]
 
     return answer
-  
-print(solution3(["marina", "josipa", "nikola", "vinko", "filipa"],["josipa", "filipa", "marina", "nikola"]))
---
+++ main.py
@@ -0,0 +1,5 @@
+from Hash.marathon import solution
+
+print(
+    solution(["mislav", "stanko", "mislav", "mislav", "mislav", "ana"],
+             ["stanko", "ana", "mislav", "mislav", "mislav"]))
