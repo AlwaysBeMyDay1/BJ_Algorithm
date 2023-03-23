f9585bf2bb9a8a510f12791bc4f92da0475a16aa
Kim Daeyeon <80505979+alwaysbemyday1@users.noreply.github.com>
2023-03-23 13:43:49 +0000

    Others_Make Weird Words

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Others.borrow_pewear import solution
+from Others.make_weird_word import solution
 
 # print(solution(5, [2, 4], [1, 3, 5]))
 # print(solution(5, [2, 4], [3]))
-print(solution(5, [4, 2 , 20], [3, 5, 21]))
+print(f">>>>{solution('    try    hello world   ')}<<<<")
