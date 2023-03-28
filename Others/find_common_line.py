e25f54f50b2971ea12e935cbbe13ba3408ea62bd
Replit user <>
2023-03-28 06:41:55 +0000

    Others_Find Common Line

--- Others/find_common_line.py
+++ Others/find_common_line.py
@@ -1,10 +1,15 @@
 def solution(lines):
+    dict = {}
     answer = 0
     for line in lines:
-        for left, right in lines:
-            if left < line[1]:
-                print(line)
-                answer += abs(min(line[1], right) - max(line[0], left))
-    return answer
+        for i in range(line[0], line[1]):
+            mid_range = (i + i + 1) / 2
+            if dict.get(mid_range):
+                dict[mid_range] += 1
+            else:
+                dict[mid_range] = 1
 
-print(solution([[0, 1], [2, 5], [3, 9]]))
+    for k, v in dict.items():
+        if v >= 2:
+            answer += 1
+    return answer
