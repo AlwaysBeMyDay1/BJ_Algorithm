b6fe42a6c45f91738160ce40a4ef3ec9882bb225
Replit user <>
2023-03-30 06:16:23 +0000

    [Others] BJ String

--- Others/bj_string.py
+++ Others/bj_string.py
@@ -1,10 +1,58 @@
+# 단어의 개수
+def print_word_num():
+    N = input()
+    print(max([int(n[::-1])for n in N.split()]))
+
+print_word_num()
+
+
+# 문자열 반복
+def repeat_string():
+    N = int(input())
+    order_list = []
+    for _ in range(N):
+        order_list.append(input())
+
+    for order in order_list:
+        print(*list(map(lambda x: x*int(order.split()[0]), order.split()[1])), sep='')
+
+        
+# 알파벳 찾기
+def find_alphabet2():
+    s=input()
+    a=list(range(97,123))
+    for i in a:
+        print (s.find(chr(i)))
+    
+from string import ascii_lowercase
+def find_alphabet():
+    alphabet_list = list(ascii_lowercase)
+    alphabet_dict = {}
+    for i in alphabet_list:
+        alphabet_dict[i] = -1
+        
+    s = input()
+    for alphabet in s:
+        alphabet_dict[alphabet] = s.index(alphabet)
+
+    print(*alphabet_dict.values())
+
+
+# 숫자의 합
+def print_stringfied_sum():
+    n = input()
+    s = input()
+    answer = 0
+    for num in s:
+        answer += int(num)        
+    print(answer)
+
+
 # 아스키 코드
-import sys
 def print_ascii():
     s = input()
     print(ord(s))
 
-print_ascii()
 
 # 문자열
 import sys
