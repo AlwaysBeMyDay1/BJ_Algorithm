8e97be10a147eba348385fc0f2f80f146272364d
Replit user <>
2023-04-01 18:03:32 +0000

    [Recursion] Merge Sort (2)

--- DFS_BFS/Recursion/merge_sort.py
+++ DFS_BFS/Recursion/merge_sort.py
@@ -36,3 +36,23 @@ if len(a)>=k:
     print(a[k-1])
 else:
     print(-1)
+
+
+
+# 다른 방법
+def find(s,e):
+    global K
+    if s == e:return
+    m=(s+e)//2
+    find(s,m)
+    find(m+1,e)
+    if K <=e-s+1:
+        J=sorted(I[s:e+1])
+        print(J[K-1])
+        exit()
+    K -= e-s+1
+
+N,K=map(int,input().split())
+I=list(map(int,input().split()))
+find(0, N-1)
+print(-1)
