56a218bb7477157b0da3c8019628c9fa89406561
Replit user <>
2023-04-02 13:12:44 +0000

    [Queue] Make Queue with Python-Deque

--- Stack_Queue/queue_with_python.py
+++ Stack_Queue/queue_with_python.py
@@ -1,3 +1,36 @@
+# <><><><><><><><><><><>Queue with Deque<><><><><><><><><><><>
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+q = deque([])
+for i in range(n):
+    s = sys.stdin.readline().split()
+    if s[0] == 'push':
+        q.append(s[1])
+    elif s[0] == 'pop':
+        if not q:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif s[0] == 'size':
+        print(len(q))
+    elif s[0] == 'empty':
+        if not q:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == 'front':
+        if not q:
+            print(-1)
+        else:
+            print(q[0])
+    elif s[0] == 'back':
+        if not q:
+            print(-1)
+        else:
+            print(q[-1])
+
 # <><><><><><><><><><><>Queue with Linked List<><><><><><><><><><><>
 """
 시간 복잡도 1로 만들기 위해서 linked list로 구현
