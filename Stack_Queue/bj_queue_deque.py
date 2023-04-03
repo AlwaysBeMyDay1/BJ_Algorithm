ec2783f5a5c0ff62033f6b5399d3d30583177b6c
Replit user <>
2023-04-03 16:30:14 +0000

    [Queue] Circular Queue

--- Stack_Queue/bj_queue_deque.py
+++ Stack_Queue/bj_queue_deque.py
@@ -1,5 +1,24 @@
-# 덱
-from collections import deque
+# 회전하는 큐
+def circulat_queue():
+    from collections import deque
+    
+    n,m,*O=map(int,open(0).read().split())
+    O=deque(O)
+    L=deque(range(1,n+1))
+    
+    i=0
+    while O:
+        o=O[0]
+        li=L.index(o)
+        ri=len(L)-li
+        mn=min(li,ri)
+        i+=mn
+        while L[0]!=o:
+            L.append(L.popleft())
+        L.popleft()
+        O.popleft()
+    
+    print(i)
 
 
 
