407eb8071df37deaf7b4b93d3a06544b734bfb4c
Replit user <>
2023-04-27 08:40:22 +0000

    [DnC] DNC (6) - Binomial Coefficient

--- Devide_and_Conquer/bj_devide_and_conquer.py
+++ Devide_and_Conquer/bj_devide_and_conquer.py
@@ -1,30 +1,53 @@
+# 이항 계수 3
+def binomial_coefficient_3():
+
+    def power(a, b):
+        if b == 0:
+            return 1
+        if b % 2:
+            return (power(a, b // 2)**2 * a) % p
+        else:
+            return (power(a, b // 2)**2) % p
+
+    p = 1000000007
+    N, K = map(int, input().split())
+    fact = [1 for _ in range(N + 1)]
+
+    for i in range(2, N + 1):
+        fact[i] = fact[i - 1] * i % p
+
+    A = fact[N]
+    B = (fact[N - K] * fact[K]) % p
+
+    print((A % p) * (power(B, p - 2) % p) % p)
+
+
 # 곱셈
 def multiple_dnc_vers2():
-    a,b,c=map(int,input().split())
-    
-    def dnc(a,b,c):
-        if b==1:
-            return a%c
-        elif b%2==0:
-            return (dnc(a,b//2,c)**2)%c
+    a, b, c = map(int, input().split())
+
+    def dnc(a, b, c):
+        if b == 1:
+            return a % c
+        elif b % 2 == 0:
+            return (dnc(a, b // 2, c)**2) % c
         else:
-            return ((dnc(a,b//2,c)**2)*a)%c
-            
-    print(dnc(a,b,c))
+            return ((dnc(a, b // 2, c)**2) * a) % c
+
+    print(dnc(a, b, c))
 
 
 # RecursionError
 def multiple_dnc_vers1():
-    a,b,c=map(int,input().split())
-    
-    def rcrs(a,aa,time,div):
-        if time==0:
+    a, b, c = map(int, input().split())
+
+    def rcrs(a, aa, time, div):
+        if time == 0:
             print(aa)
             return
-        rcrs(a,a*aa%div,time-1,div)
-        
-    rcrs(a,1,b,c)
+        rcrs(a, a * aa % div, time - 1, div)
 
+    rcrs(a, 1, b, c)
 
 
 # 종이의 개수
